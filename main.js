853c1722e500b6684b69f43a0f6944732c400ffa
stocksignal <82221987+stocksignal@users.noreply.github.com>
2021-05-15 19:39:13 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,224 @@
+const db = [{
+    'colorpickers': [
+        {
+            'name': 'Colordot',
+            'desc': 'A color picker for humans.',
+            'link': 'https://color.hailpixel.com'
+        },
+        {
+            'name': 'uiGradients',
+            'desc': 'Beautiful colored gradients.',
+            'link': 'https://uigradients.com'
+        },
+        {
+            'name': 'Colors and Fonts',
+            'desc': 'Yup. You get Colors and fonts here.',
+            'link': 'https://colorsandfonts.com'
+        },
+        {
+            'name': 'Coolors',
+            'desc': 'The super fast color schemes generator! Create the perfect palette or get inspired by thousands of beautiful color schemes.',
+            'link': 'https://coolors.co'
+        }
+    ],
+    'iconsandillustrations': [
+        {
+            'name': 'Noun Project',
+            'desc': 'Beautiful icons for your projects',
+            'link': 'http://thenounproject.com'
+        },
+        {
+            'name': 'Vector illustrations',
+            'desc': 'So many icons and illustrations to choose from.',
+            'link': 'https://icons8.com'
+        },
+        {
+            'name': 'Drawkit',
+            'desc': 'DrawKit is a collection of free, beautiful, customizable MIT licensed SVG illustrations in two styles, to use on your next website, app, or project.',
+            'link': 'https://drawkit.io'
+        },
+        {
+            'name': 'Lukasz Adam Free Illustrations',
+            'desc': 'Lukasz Adam is an independent web designer who\'s made a bunch of high-quality vector art illustrations and icons available for free. They can be used for personal and commercial projects.',
+            'link': 'https://lukaszadam.com/illustrations'
+        },
+        {
+            'name': 'Ionicons',
+            'desc': 'Ionicons are premium icons for use by designers in web, iOS, Android, and desktop apps. Built by the Ionic Framework team, they\'re all free and open source.',
+            'link': 'https://ionicons.com'
+        },
+    ],
+    'photosandvideos': [
+        {
+            'name': 'Pexels',
+            'desc': 'The best free stock photos & videos shared by talented creators.',
+            'link': 'https://pexels.com'
+        },
+        {
+            'name': 'Landing Stock',
+            'desc': 'Landing Stock is a niche site providing free stock photos that are suitable for your website landing page. Created by London-based product designer Craig Barber, it features only high-quality, clean, and minimalistic photos.',
+            'link': 'https://landingstock.com'
+        },
+        {
+            'name': 'StockSnap',
+            'desc': 'With a large selection of free high-definition stock photos, StockSnap offers a valuable resource to web designers, bloggers, and content curators.',
+            'link': 'https://StockSnap.io'
+        },
+        {
+            'name': 'Free Images',
+            'desc': 'Free Images gallery of more than 350,000 stock photos is categorized and searchable.',
+            'link': 'https://freeimages.com'
+        },
+        {
+            'name': 'Photo Creator',
+            'desc': 'CREATE YOUR REALISTIC PHOTOS IN MINUTES. Choose from thousands of models, objects, and backgrounds to perfectly tell any story.',
+            'link': 'https://icons8.com/creator'
+        },
+        {
+            'name': 'Rgbstock',
+            'desc': 'Are you looking for fabulous free stock images?  Rgbstock has a huge number of high-quality free stock photos and dazzling free graphics for illustrations, wallpapers, and backgrounds.',
+            'link': 'https://rgbstock.com/photo/nBNFJP0'
+        },
+        {
+            'name': 'Logaster logo maker',
+            'desc': 'Online logo maker.',
+            'link': 'https://logaster.com/logo'
+        },
+
+        {
+            'name': 'Animaticons',
+            'desc': 'Animaticons is a set of high-resolution animated GIFs that you can customize. They are small in file size, compatible with all major browsers, emails, and smartphones.',
+            'link': 'https://animaticons.co'
+        },
+        {
+            'name': 'Pixlr',
+            'desc': 'The Pixlr Editor is similar to Photoshop, Pixlr Express is perfect for quick fixes, and Pixler O-Matic features easy-to-use pre-made filters that can be applied to photos.',
+            'link': 'https://pixlr.com'
+        },
+        {
+            'name': 'Unsplash Source',
+            'desc': 'Simple embedding for Unsplash photos.',
+            'link': 'https://source.unsplash.com/'
+        },
+    ],
+    'fonts': [
+        {
+            'name': 'Google Fonts',
+            'desc': 'Google Fonts is a library of more than a thousand free and open source font families.',
+            'link': 'https://fonts.google.com/'
+        },
+        {
+            'name': 'Type Genius',
+            'desc': 'The site makes it simple to find the perfect font combination, offering multiple suggested pairings for any typeface selected. It also provides real-world examples of combos used on other websites. ',
+            'link': 'https://webdesignledger.com/type-genius-webapp/'
+        },
+    ],
+    'devtools': [
+        {
+            'name': 'Metatags',
+            'desc': 'This tool can help you to test and view your website\'s preview when viewed on Google, Facebook, or Twitter.',
+            'link': 'https://metatags.io'
+        },
+        {
+            'name': 'ExtractCSS',
+            'desc': 'This tool can help you to extract element Id, class, and inline styles from HTML documents and output them as CSS stylesheets.',
+            'link': 'https://extractcss.com/'
+        },
+        {
+            'name': 'WhatRuns',
+            'desc': 'This tool can help you to peek at other site\'s themes, plugins, and server info used. WhatRuns can be installed on @firefox and chrome.',
+            'link': 'https://www.whatruns.com/'
+        },
+        {
+            'name': 'Unminify',
+            'desc': 'Free tool to unminify javascript, CSS, HTML, XML, and JSON code making it readable and pretty.',
+            'link': 'https://unminify.com/'
+        },
+        {
+            'name': 'Responsively App',
+            'desc': 'This browser is designed to help you see what your website like on different technology devices at once.',
+            'link': 'https://responsively.app/'
+        },
+        {
+            'name': 'Stylify Me',
+            'desc': 'Online style guide generator.',
+            'link': 'https://stylifyme.com'
+        },
+
+        {
+            'name': 'Dribbble',
+            'desc': 'Discover the world’s top designers & creatives. Dribbble is the leading destination to find & showcase creative work and home to the world\'s best design professionals. ',
+            'link': 'https://dribbble.com'
+        },
+    ],
+    'freehosting': [
+        {
+            'name': 'Infinity Free',
+            'desc': 'Free Website Hosting Services',
+            'link': 'https://infinityfree.net'
+        },
+        {
+            'name': 'Award Space',
+            'desc': 'Free Website Hosting Services',
+            'link': 'http://awardspace.com'
+        },
+        {
+            'name': 'Free Hostia',
+            'desc': 'Free Website Hosting Services',
+            'link': 'https://freehostia.com'
+        },
+        {
+            'name': 'Google Cloud Hosting',
+            'desc': 'Free Website Hosting Services',
+            'link': 'https://cloud.google.com/solutions/web-hosting'
+        }
+    ]
+}]
+
+function makediv(arr){
+    const div = document.querySelector('.toolsList');
+    div.innerHTML = '';
+    arr.forEach(a => {
+        var temp = `<div class="flip-card">
+                        <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            ${a.name}
+                        </div>
+                        <div class="flip-card-back">
+                            <a href="${a.link}" target="_blank" rel="noopener noreferer">${a.name}</a>
+                            <p>${a.desc}</p>
+                        </div>
+                        </div>
+                    </div>`
+        div.insertAdjacentHTML('beforeend', temp);
+    });
+}
+
+function getdata(e) {
+    e.preventDefault();
+    var select = e.target.innerText;
+    select = select.replace(/\s+/g, "").toLowerCase();
+    var arr = db[0][select];
+
+    makediv(arr);
+    
+}
+
+makediv(db[0]['colorpickers']);
+
+document.querySelector('em').addEventListener('click', (e)=>{
+    e.preventDefault();
+    var div = document.querySelector('.container-inner');
+    div.style.transform = 'rotateY(180deg)';
+    
+})
+document.getElementById('gobackbtn').addEventListener('click', (e)=>{
+    e.preventDefault();
+    var div = document.querySelector('.container-inner');
+    div.style.transform = '';
+    
+})
+document.querySelector('.flip-card').addEventListener('click', (e)=>{
+    console.log(e.target);
+    e.target.style.zIndex = '100';
+})
